0f700e6af8001819b8e46c73216bc87677ba3824
codewithsabbirIYI <tauhid10030@gmial.com>
2023-09-17 12:55:30 +0600

    front end mastaring done

---
+++ functions/function.php
@@ -0,0 +1,58 @@
+<?php
+
+    // head section included 
+    function get_head(){
+        require_once('includes/head.php');
+    };
+
+    // header section included 
+    function get_header(){
+        require_once('includes/header.php');
+    };
+
+    // about section included 
+    function get_about(){
+        require_once('includes/about.php');
+    };
+
+    // service section included 
+    function get_service(){
+        require_once('includes/service.php');
+    };
+
+    // portfolio section included 
+    function get_portfolio(){
+        require_once('includes/portfolio.php');
+    };
+
+    // pricing section included 
+    function get_pricing(){
+        require_once('includes/pricing.php');
+    };
+
+    // hireMe section included 
+    function get_hireMe(){
+        require_once('includes/hireMe.php');
+    };
+
+    // testimonial section included 
+    function get_testimonial(){
+        require_once('includes/testimonial.php');
+    };
+
+    // blog section included 
+    function get_blog(){
+        require_once('includes/blog.php');
+    };
+
+    // contact section included 
+    function get_contact(){
+        require_once('includes/contact.php');
+    };
+
+    // footer section included 
+    function get_footer(){
+        require_once('includes/footer.php');
+    };
+
+?>
