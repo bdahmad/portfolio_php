cfa4e05a284d29c2e45fce7f664f0311f9040fdb
codewithsabbirIYI <tauhid10030@gmial.com>
2023-09-17 15:09:56 +0600

    backend mastering done

---
+++ admin/includes/footer.php
@@ -0,0 +1,35 @@
+
+
+        <!-- END PAGE CONTAINER-->
+        </div>
+
+    </div>
+
+    <!-- Jquery JS-->
+    <script src="../assets/admin/vendor/jquery-3.2.1.min.js"></script>
+    <!-- Bootstrap JS-->
+    <script src="../assets/admin/vendor/bootstrap-4.1/popper.min.js"></script>
+    <script src="../assets/admin/vendor/bootstrap-4.1/bootstrap.min.js"></script>
+    <!-- ../assets/admin/Vendor JS       -->
+    <script src="../assets/admin/vendor/slick/slick.min.js">
+    </script>
+    <script src="../assets/admin/vendor/wow/wow.min.js"></script>
+    <script src="../assets/admin/vendor/animsition/animsition.min.js"></script>
+    <script src="../assets/admin/vendor/bootstrap-progressbar/bootstrap-progressbar.min.js">
+    </script>
+    <script src="../assets/admin/vendor/counter-up/jquery.waypoints.min.js"></script>
+    <script src="../assets/admin/vendor/counter-up/jquery.counterup.min.js">
+    </script>
+    <script src="../assets/admin/vendor/circle-progress/circle-progress.min.js"></script>
+    <script src="../assets/admin/vendor/perfect-scrollbar/perfect-scrollbar.js"></script>
+    <script src="../assets/admin/vendor/chartjs/Chart.bundle.min.js"></script>
+    <script src="../assets/admin/vendor/select2/select2.min.js">
+    </script>
+
+    <!-- Main JS-->
+    <script src="../assets/admin/js/main.js"></script>
+
+</body>
+
+</html>
+<!-- end document-->
---
+++ admin/includes/sidebar.php
@@ -0,0 +1,31 @@
+<aside class="menu-sidebar d-none d-lg-block">
+            <div class="logo">
+                <a href="#">
+                    <img src="../assets/admin/images/icon/logo.png" alt="Cool Admin" />
+                </a>
+            </div>
+            <div class="menu-sidebar__content js-scrollbar1">
+                <nav class="navbar-sidebar">
+                    <ul class="list-unstyled navbar__list">
+                        <li class="active has-sub">
+                            <a class="js-arrow" href="#">
+                                <i class="fas fa-tachometer-alt"></i>Dashboard</a>
+                        </li>
+                        <li>
+                            <a href="chart.html">
+                                <i class="fas fa-globe"></i>Web Site</a>
+                        </li>
+                      
+                        <li class="has-sub">
+                            <a class="js-arrow" href="#">
+                                <i class="fas fa-cog"></i>Manage Header</a>
+                            <ul class="list-unstyled navbar__sub-list js-sub-list">
+                                <li>
+                                    <a href="manage_header.php">Header</a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </aside>
