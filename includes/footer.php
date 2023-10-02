1da2a086e28cbfc5a0fb86faea45458d6089164f
codewithsabbirIYI <tauhid10030@gmial.com>
2023-10-02 13:01:20 +0600

    portfolio slider added successfully

--- includes/footer.php
+++ includes/footer.php
@@ -26,6 +26,56 @@
     <!-- Meyawo js -->
     <script src="assets/js/meyawo.js"></script>
 
+    <!-- slick slider cdn  -->
+    <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+
+    
+    <!-- portfolio slick slider js code here  -->
+    <script>
+        $(document).ready(function () {
+            $('.portfolio_slide').slick({
+                infinite: true,
+                autoplay: true,
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                arrows: false,
+
+               
+                responsive: [
+                    {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                        infinite: true,
+                        dots: true
+                    }
+                    },
+                    {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2
+                    }
+                    },
+                    {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                    }
+                    // You can unslick at a given breakpoint now by adding:
+                    // settings: "unslick"
+                    // instead of a settings object
+                ]
+
+
+            });
+        });
+    </script>
+
+
 </body>
 
 </html>
--- includes/portfolio.php
+++ includes/portfolio.php
@@ -19,55 +19,54 @@ if(!$con){
             <p class="section-subtitle">What I Did ?</p>
             <h6 class="section-title mb-6">Portfolio</h6>
             <!-- row -->
-            <div class="row">
 
-
-                <?php
+            
+            <?php
                 
-                    $select_query = "SELECT * FROM portfolio";
+                $select_query = "SELECT * FROM portfolio";
 
-                    $add_data = mysqli_query($con, $select_query);
+                $add_data = mysqli_query($con, $select_query);
 
-                
-                ?>
+            
+            ?>
 
+            <div class="portfolio_slide" >
 
                 <?php 
 
                     foreach ($add_data as $key => $data) {
-                    
-                 ?>
 
-                        <!-- start item  -->
-                <div class="col-md-4">
+                ?>
+                <!-- start item -->
+                   <div class = "col-md-4">
                     <a href="<?= $data['port_live_review']?>" class="portfolio-card">
-                        <img style="width: auto; height:300px;" src="admin/uploads/<?= $data['port_image']?>" class="portfolio-card-img "
-                            alt="Download free bootstrap 4 landing page, free boootstrap 4 templates, Download free bootstrap 4.1 landing page, free boootstrap 4.1.1 templates, meyawo Landing page">
-                        <span class="portfolio-card-overlay">
-                            <span class="portfolio-card-caption">
-                                <h4><?= $data['port_name'] ?></h5>
-                                    <p class="font-weight-normal"><?= $data['port_work_description']?></p>
+                            <img style="width: auto; height:300px;" src="admin/uploads/<?= $data['port_image']?>" class="portfolio-card-img "
+                                alt="Download free bootstrap 4 landing page, free boootstrap 4 templates, Download free bootstrap 4.1 landing page, free boootstrap 4.1.1 templates, meyawo Landing page">
+                            <span class="portfolio-card-overlay">
+                                <span class="portfolio-card-caption">
+                                    <h4><?= $data['port_name'] ?></h5>
+                                        <p class="font-weight-normal"><?= $data['port_work_description']?></p>
+                                </span>
                             </span>
-                        </span>
-                        <div class="btn-group " role="group" aria-label="Basic">
+                            <div class="btn-group " role="group" aria-label="Basic">
 
-                       <a href="#"><button class=" btn btn-outline-primary mt-4">GitHub Link</button></a>
-                       <a href="#"><button class=" btn btn-outline-primary mt-4">Live Link Link</button></a>
-                          
-                        </div>
-                    </a>
-                </div>
-                <!-- end item  -->
-
+                                <a href="<?= $data['port_git_link']?>"><button class=" btn btn-outline-primary mt-4">GitHub Link</button></a>
+                                <a href="<?= $data['port_live_review']?>"><button class=" btn btn-outline-primary mt-4">Live Link Link</button></a>
+                            
+                            </div>
+                        </a>
+                   </div>
+                    <!-- end item -->
                 <?php
 
                     }
-                
-                
+
+
                 ?>
 
+                  
+               </div>
             
-                
 
 
             </div><!-- end of row -->
